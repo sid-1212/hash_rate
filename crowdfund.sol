c6756444385849c30510d4de2f5c3ae3a27da101
Siddharth Kumar <105712514+sid-1212@users.noreply.github.com>
2022-07-04 22:28:45 +0530

    Update crowdfund.sol

--- crowdfund.sol
+++ crowdfund.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "./erc20.sol";
+interface IERC20 {
+    function transfer(address, uint) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external returns (bool);
+}
 
 contract CrowdFund {
     event Launch(
@@ -18,21 +26,31 @@ contract CrowdFund {
     event Refund(uint id, address indexed caller, uint amount);
 
     struct Campaign {
+        // Creator of campaign
         address creator;
+        // Amount of tokens to raise
         uint goal;
+        // Total amount pledged
         uint pledged;
+        // Timestamp of start of campaign
         uint32 startAt;
+        // Timestamp of end of campaign
         uint32 endAt;
+        // True if goal was reached and creator has claimed the tokens.
         bool claimed;
     }
 
-    ERC20 public immutable token;
+    IERC20 public immutable token;
+    // Total count of campaigns created.
+    // It is also used to generate id for new campaigns.
     uint public count;
+    // Mapping from id to Campaign
     mapping(uint => Campaign) public campaigns;
+    // Mapping from campaign id => pledger => amount pledged
     mapping(uint => mapping(address => uint)) public pledgedAmount;
 
     constructor(address _token) {
-        token = ERC20(_token);
+        token = IERC20(_token);
     }
 
     function launch(
